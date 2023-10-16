b0be7a34e9c7eec361ab1f441613b6aef61f66b6
addingasun <116234425+addingasun@users.noreply.github.com>
2023-10-16 14:31:07 -0400

    Add files via upload
    SendingLSL.py is a direct python file of the sending lsl data method used in the .ipynb file

---
+++ SendingLSL.py
@@ -0,0 +1,49 @@
+#LSL Sending
+
+import sys
+import getopt
+
+import time
+from random import random as rand
+
+from pylsl import StreamInfo, StreamOutlet, local_clock
+
+srate = 100
+name = 'New Testing Stream!'
+type_stream = 'EEG'
+n_channels = 32
+help_string = 'SendData.py -s <sampling_rate> -n <stream_name> -t <stream_type>'
+
+def main(argv):
+
+    # first create a new stream info (here we set the name to BioSemi,
+    # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
+    # last value would be the serial number of the device or some other more or
+    # less locally unique identifier for the stream as far as available (you
+    # could also omit it but interrupted connections wouldn't auto-recover)
+    info = StreamInfo(name, type_stream, n_channels, srate, 'float32', 'myuid34234')
+
+    # next make an outlet
+    outlet = StreamOutlet(info)
+
+    print("now sending data...")
+    start_time = local_clock()
+    sent_samples = 0
+    while True:
+        elapsed_time = local_clock() - start_time
+        required_samples = int(srate * elapsed_time) - sent_samples
+        for sample_ix in range(required_samples):
+            # make a new random n_channels sample; this is converted into a
+            # pylsl.vectorf (the data type that is expected by push_sample)
+            mysample = [rand() for _ in range(n_channels)]
+            # now send it
+            outlet.push_sample(mysample)
+        sent_samples += required_samples
+        # now send it and wait for a bit before trying again.
+        time.sleep(0.01)
+    while False:
+        print("EEG data sent!")
+        break
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
